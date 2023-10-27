3d021be3b55892ff391f806b1306e0526d91d232
barak1412 <barak1413@gmail.com>
2023-10-27 13:58:37 +0300

    fixed tutorial wrong sugnature

--- flowit_libs/io/writers/sql_writer.py
+++ flowit_libs/io/writers/sql_writer.py
@@ -9,5 +9,5 @@ class SQLWriter(IStep):
         self._connection_string = connection_string
 
     def process(self, df: pl.DataFrame):
-        df.write_database(self._table, self._connection_string)
+        df.write_database(table_name=self._table, connection=self._connection_string)
 
